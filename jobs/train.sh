ed2c90980b725417a062c85b0d8ae37efefdf0a8
wangyingbin <yingbin.wang@geely.com>
2020-03-19 14:35:37 +0800

    .

--- jobs/train.sh
+++ jobs/train.sh
@@ -11,7 +11,8 @@ f_log=log.txt
 f_out=out.txt
 f_model=model.txt
 eta=0.000000001
-dim=10
+dim=8
 
 ${bin} -task c -method sgd -train ${f_train} -test ${f_test} -dim "1,1,${dim}" -regular '0,0,0.01' -learn_rate ${eta} -init_stdev 0.5 -iter ${iter} -rlog ${f_log} -out ${f_out} -save_model ${f_model}
+
 # ${bin} -task c -method mcmc -train ${f_train} -test ${f_test} -dim '1,1,5' -iter ${iter} -rlog ${f_log} -out ${f_out}
